c76e57becf9243c7eb183b387f2c3a651c390dba
Heaguke <tom.petitprez.nsi@gmail.com>
2025-01-22 18:47:45 +0100

    Modif 4
    j'ai supprimer ce qui me servait a rien j'ai réussi sur java de reprendre la cle de acceuil quand je clique dessus et donc donné les info du film et j'ai tt bien arrangé avec le css

--- javaMovie.js
+++ javaMovie.js
@@ -2,13 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const selectedMovieId = sessionStorage.getItem('selectedFilmId');
     console.log(selectedMovieId)
     if (selectedMovieId) {
-        // fetchAndDisplaySelectedMovie(selectedMovieId);
         fetch(`https://www.omdbapi.com/?apikey=d957911b&i=${selectedMovieId}`)
             .then(response => response.json())
             .then(data => {
             document.getElementById('film').innerHTML = `<h1>${data.Title}</h1>`;
             document.getElementById('affiche').innerHTML = `<img src="${data.Poster}" alt="Affiche de ${data.Title}" id="photo">`;
-            document.getElementById('plot').innerHTML = `<p id="plotes">${data.Plot}</p>`;
+            document.getElementById('plot').innerHTML = `<p id="plot">Résumé : ${data.Plot}</p>`;
             document.getElementById('acteurs').innerHTML = `<p id="acteurs">Acteurs : ${data.Actors}</p>`;
             document.getElementById('genre').innerHTML = `<p id="genre">Genres : ${data.Genre}</p>`;
             document.getElementById('notes').innerHTML = `<p id="notes">Notes : ${data.imdbRating}</p>`;
@@ -19,17 +18,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-// fetch(`https://www.omdbapi.com/?apikey=d957911b&i=${selectedMovieId}`)
-//     .then(response => response.json())
-//     .then(data => {
-//         document.getElementById('film').innerHTML = `<h1>${data.Title}</h1>`;
-//         document.getElementById('affiche').innerHTML = `<img src="${data.Poster}" alt="Affiche de ${data.Title}" id="photo">`;
-//         document.getElementById('plot').innerHTML = `<p id="plotes">${data.Plot}</p>`;
-//         document.getElementById('acteurs').innerHTML = `<p id="acteurs">Acteurs : ${data.Actors}</p>`;
-//         document.getElementById('genre').innerHTML = `<p id="genre">Genres : ${data.Genre}</p>`;
-//         document.getElementById('notes').innerHTML = `<p id="notes">Notes : ${data.imdbRating}</p>`;
-//     })
-// .catch(error => console.error("Erreur lors de la récupération des données :", error));
 
 
 
