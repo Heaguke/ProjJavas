8a37617583aece091ba107561510c49cacbe6323
Heaguke <tom.petitprez.nsi@gmail.com>
2025-01-22 16:19:51 +0100

    Modif 3
    modif de tt

--- javaMovie.js
+++ javaMovie.js
@@ -1,22 +1,36 @@
-const filmId = sessionStorage.getItem('selectedFilmId');
+document.addEventListener('DOMContentLoaded', () => {
+    const selectedMovieId = sessionStorage.getItem('selectedFilmId');
+    console.log(selectedMovieId)
+    if (selectedMovieId) {
+        // fetchAndDisplaySelectedMovie(selectedMovieId);
+        fetch(`https://www.omdbapi.com/?apikey=d957911b&i=${selectedMovieId}`)
+            .then(response => response.json())
+            .then(data => {
+            document.getElementById('film').innerHTML = `<h1>${data.Title}</h1>`;
+            document.getElementById('affiche').innerHTML = `<img src="${data.Poster}" alt="Affiche de ${data.Title}" id="photo">`;
+            document.getElementById('plot').innerHTML = `<p id="plotes">${data.Plot}</p>`;
+            document.getElementById('acteurs').innerHTML = `<p id="acteurs">Acteurs : ${data.Actors}</p>`;
+            document.getElementById('genre').innerHTML = `<p id="genre">Genres : ${data.Genre}</p>`;
+            document.getElementById('notes').innerHTML = `<p id="notes">Notes : ${data.imdbRating}</p>`;
+    })
+.catch(error => console.error("Erreur lors de la récupération des données :", error));
+    } else {
+        console.error('Aucun film trouvé');
+    }
+});
 
-if (filmId) {
-    fetch(`https://www.omdbapi.com/?apikey=d957911b&i=${filmId}`)
-        .then(response => response.json())
-        .then(data => {
-            const filmDetailsContainer = document.getElementById('filmDetails');
-            const filmTitle = document.createElement('h1');
-            filmTitle.textContent = data.Title;
-            const filmPoster = document.createElement('img');
-            filmPoster.src = data.Poster;
-            filmPoster.alt = data.Title;
-            const filmPlot = document.createElement('p');
-            filmPlot.textContent = data.Plot;
-            filmDetailsContainer.appendChild(filmTitle);
-            filmDetailsContainer.appendChild(filmPoster);
-            filmDetailsContainer.appendChild(filmPlot);
-        })
-        .catch(err => console.error('Erreur API:', err));
-} else {
-    console.error('Aucun ID de film trouvé dans le sessionStorage');
-}
+// fetch(`https://www.omdbapi.com/?apikey=d957911b&i=${selectedMovieId}`)
+//     .then(response => response.json())
+//     .then(data => {
+//         document.getElementById('film').innerHTML = `<h1>${data.Title}</h1>`;
+//         document.getElementById('affiche').innerHTML = `<img src="${data.Poster}" alt="Affiche de ${data.Title}" id="photo">`;
+//         document.getElementById('plot').innerHTML = `<p id="plotes">${data.Plot}</p>`;
+//         document.getElementById('acteurs').innerHTML = `<p id="acteurs">Acteurs : ${data.Actors}</p>`;
+//         document.getElementById('genre').innerHTML = `<p id="genre">Genres : ${data.Genre}</p>`;
+//         document.getElementById('notes').innerHTML = `<p id="notes">Notes : ${data.imdbRating}</p>`;
+//     })
+// .catch(error => console.error("Erreur lors de la récupération des données :", error));
+
+
+
+  
